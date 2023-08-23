79177d270a16d4eee684dbc10c010cb02f6da14c
Steen Hjalmar Larsen <sthl@youmail.dk>
2023-08-23 20:42:54 +0200

    new structor

--- calculater/assets/scripts/main.js
+++ calculater/assets/scripts/main.js
@@ -13,19 +13,4 @@ function showInput(inputDOM, resultDOM) {
 	});
 }
 
-function showType(text, resultDOM) {
-	resultDOM.innerHTML = text;
-}
-
-// 
-let a_undefined; // undefined
-let b_undefined = undefined; // undefined
-
-const type = typeof b_undefined;
-
-showType(type, resultDOM);
-
-// const t2 = 2;
-// console.log(typeof parseFloat(t1));
-// let t3 = t1 + t2;
-// console.log(t3);
+showInput(inputDOM, resultDOM);
